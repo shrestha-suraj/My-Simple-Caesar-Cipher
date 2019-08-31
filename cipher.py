718f617a4322cd5965db00c082d6eab95583b342
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-08-31 11:10:02 -0500

    Done with project. Need to submit readme file

--- cipher.py
+++ cipher.py
@@ -1,29 +1,39 @@
 message=input('Enter the message you want to encode: ')
-key=34190725
-print("This is the message: "+message)
+mykey=34190725
+key=input("Enter the 8 bit key for encryption and decryption (Example: 12345678):  ")
 
 def encrypter(message,key):
     bit_keys=[int(x) for x in str(key)]
-    print("Log Bit_Keys: " + str(bit_keys))
     message_array=[str(y) for y in str(message)]
-    print("Log Message_Array: " + str(message_array))
     bit_control=8
     encoded_message=""
     for i in range(0,len(message)):
         each_key=bit_keys[i % bit_control]
-        print("Log Each_Key: "+str(each_key))
         each_char=message_array[i]
-        print("Log Each_Char: "+str(each_char))
         char_ascii=ord(each_char)
-        print("Log char_ascii: "+str(char_ascii))
         if (each_key % 2 == 0):
             char_ascii+=each_key
         else:
             char_ascii-=each_key
         encoded_message+=str(chr(char_ascii))
+    return encoded_message
 
+def decrypter(code,key):
+    bit_keys=[int(x) for x in str(key)]
+    message_array=[str(y) for y in str(code)]
+    bit_control=8
+    decoded_message=""
+    for i in range(0,len(code)):
+        each_key=bit_keys[i % bit_control]
+        each_char=message_array[i]
+        char_ascii=ord(each_char)
+        if (each_key % 2 != 0):
+            char_ascii+=each_key
+        else:
+            char_ascii-=each_key
+        decoded_message+=str(chr(char_ascii))
+    return decoded_message
 
-    print (encoded_message)
-
-
-encrypter(message,key)
+code=encrypter(message,key)
+print("Message encryption successful.\nEncrypted Code: "+str(code))
+print("\n\nMessage decryption successful.\nDecrypted Message: "+str(decrypter(code,key)))
